ee9e8e38b43527ec87cd07271f6798985d781fae
Imran Merchant <imerchant@outlook.com>
2019-12-29 16:00:58 -0600

    Add Day 06 puzzle 2 solution

--- src/AdventOfCode2019/Day06/OrbitMap.cs
+++ src/AdventOfCode2019/Day06/OrbitMap.cs
@@ -31,6 +31,37 @@ namespace AdventOfCode2019.Day06
             Map[body1].OrbitedBy.Add(Map[body2]);
         }
 
+        public int GetHopsBetween(string id1, string id2)
+        {
+            var body1 = Map[id1];
+            var body2 = Map[id2];
+
+            var path1 = GetPath(body1);
+            var path2 = GetPath(body2);
+
+            var shared = path1.Intersect(path2).ToList();
+
+            var uniques = new HashSet<Body>(path1);
+            uniques.UnionWith(path2);
+            uniques.ExceptWith(shared);
+
+            return uniques.Count;
+
+            Stack<Body> GetPath(Body body)
+            {
+                var stack = new Stack<Body>();
+                var thisBody = body.Orbits.First();
+                do
+                {
+                    stack.Push(thisBody);
+                    thisBody = thisBody.Orbits.FirstOrDefault();
+                }
+                while (thisBody != null);
+
+                return stack;
+            }
+        }
+
         public IEnumerator<Body> GetEnumerator()
         {
             return Map.Values.GetEnumerator();
@@ -56,5 +87,18 @@ namespace AdventOfCode2019.Day06
             Orbits = new List<Body>();
             OrbitedBy = new List<Body>();
         }
+
+        public override string ToString() => Id;
+
+        public override bool Equals(object obj) => Equals(obj as Body);
+
+        public bool Equals(Body body)
+        {
+            if (body == null) return false;
+            if (ReferenceEquals(this, body)) return true;
+            return body.Id == Id;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
     }
 }
--- test/AdventOfCode2019.Tests/Solutions/Day06Solutions.cs
+++ test/AdventOfCode2019.Tests/Solutions/Day06Solutions.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode2019.Day06;
 using AdventOfCode2019.Inputs;
@@ -23,6 +24,16 @@ namespace AdventOfCode2019.Tests.Solutions
             map.Connections.Should().Be(139597);
         }
 
+        [Fact]
+        public void Puzzle2_CountsHopsBetweenYOUAndSAN()
+        {
+            var orbits = Input.Day06Parse(Input.Day06);
+
+            var map = new OrbitMap(orbits);
+
+            map.GetHopsBetween("SAN", "YOU").Should().Be(286);
+        }
+
         const string Example1 =
 @"COM)B
 B)C
@@ -45,6 +56,31 @@ K)L";
 
             map.Should().HaveCount(12);
             map.Connections.Should().Be(42);
+        }
+
+        const string Example2 =
+@"COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN";
+
+        [Fact]
+        public void Example2_CountsHopsCorrectly()
+        {
+            var orbits = Input.Day06Parse(Example2);
+
+            var map = new OrbitMap(orbits);
+
+            map.GetHopsBetween("SAN", "YOU").Should().Be(4);
         }
     }
 }
